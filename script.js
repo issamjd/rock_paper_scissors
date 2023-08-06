70a8b6881347358ae5b15f277b284ee836af152f
issamjd <88335660+issamjd@users.noreply.github.com>
2023-08-06 14:27:04 +0300

    add game logic

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log("Hello World")
+
+let choices = ['rock', 'paper', 'scissors']
+let result = 0
+
+function getComputerChoice(){
+    let randomNB = Math.floor(Math.random() * 3)
+    return choices[randomNB]
+}
+
+function playGame(compChoice, playerChoice){
+    let returnMSG = ""
+    if (compChoice == playerChoice){
+        returnMSG = returnMSG + "It's a TIE!! " + compChoice + " matches " + playerChoice
+    }
+    else{
+        if ((compChoice == 'rock' && playerChoice == 'scissors')||
+            (compChoice == 'paper' && playerChoice == 'rock')||
+            (compChoice == 'scissors' && playerChoice == 'paper')){
+
+            returnMSG = returnMSG + "You Lose!! " + compChoice + " beats " + playerChoice
+            result =- 1
+        }
+        
+        else if ((playerChoice == 'rock' && compChoice == 'scissors')||
+                 (playerChoice == 'paper' && compChoice == 'rock')||
+                 (playerChoice == 'scissors' && compChoice == 'paper')){
+
+            returnMSG = returnMSG + "You WIN!! " + playerChoice + " beats " + compChoice
+            result =+ 1                
+        }
+    }
+    return returnMSG
+}
+
+function game(){
+    for(let i = 0; i < 5; i++){
+        let compChoice = getComputerChoice()
+        let playerChoice = prompt("enter your choice").toLowerCase()
+        console.log(playGame(compChoice, playerChoice))
+    }
+
+    if (result >= 0){
+        result = 0
+        return "You Win"
+    }
+    else {
+        result = 0
+        return "You Lose"
+    }
+}
+
+console.log(game())
+
+
+
+
